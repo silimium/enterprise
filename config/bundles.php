01526cac5325b18d242225cab737d72772cda4f8
Tseho <quentin.favrie@gmail.com>
2021-06-29 23:52:43 +0200

    enabled task & task-archive bundles

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Silimium\UserBundle\SilimiumUserBundle::class => ['all' => true],
+    Silimium\TaskBundle\SilimiumTaskBundle::class => ['all' => true],
+    Silimium\TaskArchiveBundle\SilimiumTaskArchiveBundle::class => ['all' => true],
 ];
